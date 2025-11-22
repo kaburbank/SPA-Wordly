54eddeb1fafe1b9558b96138c26169c6fdd57c1a
kaburbank <kaburbank@gmail.com>
2025-11-22 08:03:30 -0700

    test attempts

--- index.test.js
+++ index.test.js
@@ -1,4 +1,39 @@
-const { displayResults, playAudio, clearResults, showError } = require('./index'); // Import the function
+const { fetchWordData, playAudio, clearResults, showError } = require('./index'); // Import the function
 
-describe('hello', () => {
-    const data = 
+describe('fetchWordData', () => {
+     it("fetch the word data", () => {
+        const data = "hello";
+        const result = fetchWord(data);
+        expect(fetchWordData(data)).toBe(data[0]);
+    });
+     it("show an error message", () => {
+        const data = "00";
+        const result = fetchWord(data);
+        expect(fetchWordData(data)).toBe("00 is not found in the dictionary");
+    });
+
+describe('playAudio', () => {
+    it("play audio", () => {
+        const audio = "hello";
+        const result = playAudio();
+        expect(fetchWordData(data)).toBe(audioButton.dataset.audioUrl);
+    });
+    it("play audio", () => {
+        const audio = "00";
+        const result = playAudio();
+        expect(fetchWordData(data)).toBe('Unable to play audio');
+    });
+
+describe('clearResults', () => {
+     it("clear results", () => {
+        const data = "";
+        const result = clearResults;
+        expect(clearResults())).toBe("");
+    });
+
+describe('showError', () => {
+     it("show error", () => {
+        const message = errorMessage.textContent;
+        const result = true;
+        expect(showError(message))).toBe(errorMessage.textContent);
+    });
